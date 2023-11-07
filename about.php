0ad94a3b72e3947a5ba6f5eea0c5c2b50b65c502
Kemoi <aaronkipkirui015@gmail.com>
2023-11-07 10:12:38 +0300

    Initial Commit

---
+++ about.php
@@ -0,0 +1,105 @@
+<?php
+require ("includes/common.php");
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Shoppy | Shopping Site for Men</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Delius Swash Caps' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Andika' rel='stylesheet'>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body style="overflow-x:hidden; padding-bottom:100px;">
+  <?php
+        include 'includes/header_menu.php';
+    ?>
+  <div>
+    <div class="container mt-5 ">
+      <div class="row justify-content-around">
+        <div class="col-md-5 mt-3">
+          <h3 class="text-warning pt-3 title">Who We Are?</h3>
+          <hr />
+          <img
+            src="https://images.unsplash.com/photo-1490578474895-699cd4e2cf59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&h=400&q=80"
+            class="img-fluid d-block rounded mx-auto image-thumbnail">
+          <p class="mt-2">Shoppy's men's wear online shop is a stylish and convenient destination for fashion-forward individuals seeking the latest trends and timeless classics. With a user-friendly interface, this e-commerce platform offers a seamless shopping experience, allowing customers to explore a diverse range of men's clothing, from sharp suits and casual wear to accessories and footwear. The shop showcases an array of well-curated collections, ensuring that every shopper can find their unique style, whether it's for a formal event or a casual outing. With an emphasis on quality and affordability, Shopify's men's wear shop caters to a wide range of tastes and budgets, providing a one-stop solution for the modern man's fashion needs. It combines an easy-to-navigate website with secure payment options, making it a trusted choice for those seeking both style and convenience in their online shopping experience.</p>
+        </div>
+        <div class="col-md-5 mt-3">
+          <span class="text-warning pt-3">
+            <h1 class="title">LIVE SUPPORT</h1>
+            <h3>24/7 days Live Technical Support</h3>
+          </span>
+          <hr>
+          <p>Our 24-hour live support team is a dedicated and highly responsive group of professionals committed to providing round-the-clock assistance and expertise to our customers. They stand ready to address queries, resolve issues, and offer guidance at any time, ensuring uninterrupted support regardless of the hour. With a wealth of knowledge and a passion for customer satisfaction, our team is not only prompt but also adept at solving diverse problems, guaranteeing a seamless and reliable customer experience that's available 24/7, reflecting our unwavering commitment to our customers' needs.
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container pb-3">
+  </div>
+  <div class="container mt-3 d-flex justify-content-center card pb-3 col-md-6">
+
+    <form class="col-md-12" action="https://formspree.io/EnterYourEmail" method="POST" name="_next">
+      <h3 class="text-warning pt-3 title mx-auto">Contact Form</h3>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Email"
+          name="email">
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="5"></textarea>
+      </div>
+      <input type="hidden" name="_next" value="http://localhost/foody/about.php" />
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+
+
+  </div>
+  <!--footer -->
+  <?php include 'includes/footer.php'?>
+  <!--footer end-->
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script>
+  $(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+  });
+  $(document).ready(function () {
+
+    if (window.location.href.indexOf('#login') != -1) {
+      $('#login').modal('show');
+    }
+
+  });
+</script>
+<?php if(isset($_GET['error'])){ $z=$_GET['error']; echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#signup').modal('show');
+});
+</script>"; echo "
+<script type='text/javascript'>alert('".$z."')</script>";} ?>
+<?php if(isset($_GET['errorl'])){ $z=$_GET['errorl']; echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#login').modal('show');
+});
+</script>"; echo "
+<script type='text/javascript'>alert('".$z."')</script>";} ?>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Shoppy | All Rights Reserved </b></span></div>
+</footer>
+    
